871ef2b0ee103d265921c3b83b51a1e50d5949ac
wangdong <260287076@qq.com>
2015-04-16 19:16:33 +0800

    再提交

---
+++ GitTest/GitTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitTest
+//
+//  Created by wangdong on 15/4/16.
+//  Copyright (c) 2015年 wangdong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
